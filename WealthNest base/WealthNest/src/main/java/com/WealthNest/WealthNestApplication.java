dd16b97d554ef343c740edb3707167cc30b90893
Daniel <161328671+GeniusUngrateful@users.noreply.github.com>
2025-07-30 13:32:15 +0200

    Add files via upload

---
+++ WealthNest base/WealthNest/src/main/java/com/WealthNest/WealthNestApplication.java
@@ -0,0 +1,13 @@
+package com.WealthNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthNestApplication.class, args);
+	}
+
+}
